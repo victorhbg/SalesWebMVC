72f67a71ebeac3f56802789fb018c3167213010e
victorhbg <victorhbg84@yahoo.com.br>
2021-07-13 18:09:31 -0300

    Implementing grouping search

--- SalesWebMvc/Models/SalesRecord.cs
+++ SalesWebMvc/Models/SalesRecord.cs
@@ -9,7 +9,6 @@ namespace SalesWebMvc.Models
     {
         public int Id { get; set; }
 
-        [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
         public DateTime Date { get; set; }
 
